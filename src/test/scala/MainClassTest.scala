201bdb0528e7f4ecf79d4787984cc4e662d1aa38
Tom Liddle <tom.liddle@gmail.com>
2016-02-01 12:36:56 +0000

    Added more drawLine tests

--- src/test/scala/MainClassTest.scala
+++ src/test/scala/MainClassTest.scala
@@ -25,6 +25,36 @@ class MainClassTest extends WordSpec with Matchers with BeforeAndAfterEach{
 				canvas.drawLine(1, 1, 1, 1) should equal(Canvas(array))
 			}
 
+			"draw a horizontal line of length 4" in {
+				val canvas = Canvas.create(4, 4)
+
+				val array =
+					Seq(
+						Seq.fill[Char](6)('-'),
+						Seq('|', ' ', ' ', ' ', ' ', '|'),
+						Seq('|', 'X', 'X', 'X', 'X', '|'),
+						Seq('|', ' ', ' ', ' ', ' ', '|'),
+						Seq('|', ' ', ' ', ' ', ' ', '|'),
+						Seq.fill[Char](6)('-'))
+
+				canvas.drawLine(1, 2, 4, 2) should equal(Canvas(array))
+			}
+
+			"draw a vertical line of length 4" in {
+				val canvas = Canvas.create(4, 4)
+
+				val array =
+					Seq(
+						Seq.fill[Char](6)('-'),
+						Seq('|', ' ', ' ', ' ', 'X', '|'),
+						Seq('|', ' ', ' ', ' ', 'X', '|'),
+						Seq('|', ' ', ' ', ' ', 'X', '|'),
+						Seq('|', ' ', ' ', ' ', 'X', '|'),
+						Seq.fill[Char](6)('-'))
+
+				canvas.drawLine(4, 1, 4, 4) should equal(Canvas(array))
+			}
+
 		}
 	}
 }
