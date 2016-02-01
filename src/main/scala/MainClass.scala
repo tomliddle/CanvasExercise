f3fc6207c4cf6abc8c8d41ac88fe4e2c04feaf29
Tom Liddle <tom.liddle@gmail.com>
2016-02-01 10:58:46 +0000

    Added Canvas class and drawline and draw rectangle methods
    No error checking as yet.
    Canvas contains a 2d mutable char array. May change this in the future.

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -1,6 +1,29 @@
 
 
+class Canvas(width: Int, height: Int) {
+
+	private val charArray: Array[Array[Char]] = Array.fill[Char](width, height){' '}
+
+	def drawLine(x1: Int, y1: Int, x2: Int, y2: Int) = {
+		for (x <- x1 to x2; y <- y1 to y2) {
+			charArray(x)(y) = 'X'
+		}
+	}
+
+	def drawRectangle(x1: Int, y1: Int, x2: Int, y2: Int) = {
+		drawLine(x1, y1, x2, y1)
+		drawLine(x1, y2, x2, y2)
+		drawLine(x1, y1, x1, y2)
+		drawLine(x2, y1, x2, y2)
+	}
+
+}
+
 object MainClass {
+
+	var canvas = new Canvas(0, 0)
+
+
 	def main(args: Array[String]): Unit = {
 
 		while (true) {
@@ -8,10 +31,10 @@ object MainClass {
 			if (command.length < 1) commandNotValid()
 
 			command(0) match {
-				case "C" => createCanvas(5, 3)
-				case "L" =>
-				case "R" =>
-				case "B" =>
+				case "C" => canvas = new Canvas(6, 6)
+				case "L" => canvas.drawLine(1, 3, 1, 3)
+				case "R" => canvas.drawRectangle(1, 5, 1, 5)
+				case "B" => 
 				case "Q" => System.exit(0)
 				case _ => commandNotValid()
 			}
@@ -21,8 +44,5 @@ object MainClass {
 
 	private def commandNotValid() = println("Please enter a valid command")
 
-	private def createCanvas(width: Int, height: Int): Unit = {
-
-	}
 }
 
